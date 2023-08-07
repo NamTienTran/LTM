45f4cb1f5823fa7f41e593fe887f5753c70d369c
Tran Nam Tien <tien.tran17051988@hcmut.edu.vn>
2023-08-07 11:02:57 +0700

    create project

---
+++ LTM/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LTM.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult ListUser()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult ForgotPassword() 
+        { 
+            return View(); 
+        }
+
+        public ActionResult ResetPassword()
+        {
+            return View();
+        }
+    }
+}
